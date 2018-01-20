7ac619d7fdd49e9f8b83851ebbeac02efae191fd
learnless <b13502518214@163.com>
2018-01-20 14:13:06 +0800

    Java实战

--- src/main/java/org/learnless/chap05/StreamNumberic.java
+++ src/main/java/org/learnless/chap05/StreamNumberic.java
@@ -3,6 +3,9 @@ package org.learnless.chap05;
 import org.learnless.model.Dish;
 
 import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * 数值流
@@ -13,16 +16,25 @@ public class StreamNumberic {
         List<Dish> menu = Dish.menu;
 
         int sum = menu.stream()
-                .map(Dish::getCalories)
+                .map(Dish::getCalories) //对象流
                 .reduce(0, Integer::sum);
 
-        //原始流
         //映射到数值流
         int sum1 = menu.stream()
                 .mapToInt(Dish::getCalories)
                 .sum();
 
+        //转换为对象流
+        IntStream intStream = menu.stream().mapToInt(Dish::getCalories);
+        Stream<Integer> integerStream = intStream.boxed();  //将数值流转换为对象流
+
+        //没有最大值，提供一个默认值
+        OptionalInt optionalInt = menu.stream()
+                .mapToInt(Dish::getCalories)
+                .max();
+        int max = optionalInt.orElse(1);
+
         System.out.println(sum);
-        System.out.println(sum1);
+        System.out.println(max);
     }
 }
